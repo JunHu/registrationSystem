acaf01c1a57696305b5c0cbf09798359f63d50f8
chousan1989 <347096491@qq.com>
2014-10-12 21:06:33 +0800

    wish

--- home/ajax.py
+++ home/ajax.py
@@ -12,11 +12,16 @@ def getCollege(request,apartment):
     return simplejson.dumps({"college":college})
 
 @dajaxice_register
-def exportData(request, choose):
+def exportData(request, choose,wish):
     print "haha"
     message = ""
     choose = int(choose)
-    obj = ApplyInfo.objects.filter(Q(wish_first = choose) | Q(wish_second = choose))
+    wish=int(wish)
+    if(wish==0):
+        query=Q(wish_first=choose)
+    else:
+        query=Q(wish_second=choose)
+    obj = ApplyInfo.objects.filter(query)
     class_type = CLASS_CHOICES[choose][1] 
     path = info_xls_baseinformation(request,obj,class_type)
     return simplejson.dumps({"message": message,"path":path})
